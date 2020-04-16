7b26d300bc51af35ca58a1caf22bb208a04b4dd8
rodrigo <rodrigoluisguzman@gmail.com>
2020-04-15 23:09:22 -0300

    Agregar archivos de proyecto.

---
+++ TP2_Guzman/Articulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2_Guzman
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public List<Marca> Marcas { get; set; }
+        public List<Categoria> Categorias { get; set; }
+        public string Imagen { get; set; }
+        public float Precio { get; set; }
+        public List<Proveedor>Proveedores { get; set; }
+    }
+}
---
+++ TP2_Guzman/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2_Guzman
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Dni { get; set; }
+        public string Celular { get; set; }
+        public string Mail { get; set; }
+    }
+}
